e4eee99219f6734e5b9aead1ed5ba2a9fa8735a5
Kwasi Darkwa <kwasidarkwa@digawana.bhp.org.bw>
2017-07-21 15:16:01 +0200

    section 5

--- bais_subject/choices.py
+++ bais_subject/choices.py
@@ -2,3 +2,8 @@ MALE_FEMALE = (
     ('MALE', 'MALE'),
     ('FEMALE', 'FEMALE')
 )
+
+YES_NO = (
+    (YES, _(YES)),
+    (NO, _(NO)),
+)
--- bais_subject/models/section5.py
+++ bais_subject/models/section5.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-from ..choices import import 
+from ..choices import YES_NO
+
+
+class Section5:
+
+    """ Knowledge About HIV and AIDS and TB
+     and level of Access to Interventions
+     """
+    hiv_and_aids_awareness = models.CharField(
+        verbose_name="Have you ever heard of HIV or an illness called AIDS?",
+        max_length=35,
+        choices=YES_NO,
+        help_text="",
+    )
