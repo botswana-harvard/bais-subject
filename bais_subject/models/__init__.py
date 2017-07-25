21a4764df414118a6e34d2727d1082621fed7e62
aseokana <aseokana@bhp.org.bw>
2017-07-25 12:23:24 +0200

    adding model classes

--- bais_subject/models/__init__.py
+++ bais_subject/models/__init__.py
@@ -2,3 +2,6 @@ from .section1 import Section1
 from .section2 import Section2
 from .section3 import Section3
 from .section4 import Section4
+from .household_questionnaire import (
+    HouseholdQuestionnaireName,
+    HouseholdQuestionnaireAnswers)
