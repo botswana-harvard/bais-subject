c305943014c56e7b4d9899a5fd25cd27662f7c5f
Kwasi Darkwa <kwasidarkwa@digawana.bhp.org.bw>
2017-07-25 12:28:18 +0200

    init update

--- bais_subject/models/__init__.py
+++ bais_subject/models/__init__.py
@@ -2,6 +2,10 @@ from .section1 import Section1
 from .section2 import Section2
 from .section3 import Section3
 from .section4 import Section4
+from .section5 import Section5
+from .section6 import Section6
+from .section7 import Section7
+from .section8 import Section8
 from .household_questionnaire import (
     HouseholdQuestionnaireName,
     HouseholdQuestionnaireAnswers)
