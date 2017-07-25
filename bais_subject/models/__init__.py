6fb304e18b691bdc0320c315267c3a98c91da0c2
aseokana <aseokana@bhp.org.bw>
2017-07-25 12:45:09 +0200

    amending classes

--- bais_subject/models/__init__.py
+++ bais_subject/models/__init__.py
@@ -6,10 +6,7 @@ from .section5 import Section5
 from .section6 import Section6
 from .section7 import Section7
 from .section8 import Section8
-<<<<<<< Updated upstream
 from .household_questionnaire import (
     HouseholdQuestionnaireName,
     HouseholdQuestionnaireAnswers)
-=======
-from .household_member import HouseHoldMember
->>>>>>> Stashed changes
+from .household_member import HouseholdMember
