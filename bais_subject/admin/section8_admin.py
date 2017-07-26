34ff005c4c16777d3c5c7076f38f7d0ff45ed89f
Kwasi Darkwa <kwasidarkwa@digawana.bhp.org.bw>
2017-07-26 16:26:16 +0200

    update

--- bais_subject/admin/section8_admin.py
+++ bais_subject/admin/section8_admin.py
@@ -5,3 +5,15 @@ from edc_base.modeladmin_mixins import audit_fieldset_tuple
 from ..admin_site import bais_subject_admin
 from ..forms import Section8Form
 from ..models import Section8
+
+
+@admin.register(Section8, site=bais_subject_admin)
+class Section8Admin(admin.ModelAdmin):
+
+    form = Section8Form
+
+    radio_fields = {
+        'tb_current_treatment': admin.VERTICAL,
+        'tb_treatment_source': admin.VERTICAL,
+        'tb_previous_treatment': admin.VERTICAL,
+    }
