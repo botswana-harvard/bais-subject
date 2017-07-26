dee0964445dd4cfd8e11718c39fa25c573058cdd
aseokana <aseokana@bhp.org.bw>
2017-07-26 09:00:22 +0200

    adding admin site file

---
+++ bais_subject/admin_site.py
@@ -0,0 +1,11 @@
+from django.contrib.admin import AdminSite as DjangoAdminSite
+
+
+class AdminSite(DjangoAdminSite):
+    site_title = 'Bais Subject'
+    site_header = 'Bais Subject'
+    index_title = 'Bais Subject'
+    site_url = '/bais_subject/list/'
+
+
+ambition_subject_admin = AdminSite(name='bais_subject_admin')
