6610adbf79bcb4fe07341b3d8aea54fb7034d815
aseokana <aseokana@bhp.org.bw>
2017-07-26 14:26:50 +0200

    adding form classes

--- bais_subject/forms/__init__.py
+++ bais_subject/forms/__init__.py
@@ -1,1 +1,5 @@
+from .household_questionnaire_form import HouseholdQuestionnaireForm
 from .section1_form import Section1Form
+from .section2_form import Section2Form
+from .section3_form import Section3Form
+from .section4_form import Section4Form
