b8db3fc7d5b6236c0bf588a0030c0faf0277f1f2
aseokana <aseokana@bhp.org.bw>
2017-08-08 09:18:04 +0200

    importing form validators

--- bais_subject/forms/section3_form.py
+++ bais_subject/forms/section3_form.py
@@ -1,9 +1,18 @@
 from django import forms
 
+from bais_subject_form_validators.form_validations import (
+    Section3FormValidator)
+
 from ..models import Section3
 
 
 class Section3Form(forms.ModelForm):
+
+    def clean(self):
+        cleaned_data = super().clean()
+        cleaned_data = Section3FormValidator(
+            cleaned_data=cleaned_data).clean()
+        return cleaned_data
 
     class Meta:
         model = Section3
