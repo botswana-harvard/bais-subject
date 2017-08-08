dddb177d3ca3df72583230b431e2331926eccc99
aseokana <aseokana@bhp.org.bw>
2017-08-08 09:12:29 +0200

    importing form validators

--- bais_subject/forms/household_questionnaire_form.py
+++ bais_subject/forms/household_questionnaire_form.py
@@ -1,10 +1,19 @@
 from django import forms
 
+from bais_subject_form_validators.form_validations import (
+    HouseholdQuestionnaireFormValidator)
+
 
 from ..models import HouseholdQuestionnaire
 
 
 class HouseholdQuestionnaireForm(forms.ModelForm):
+
+    def clean(self):
+        cleaned_data = super().clean()
+        cleaned_data = HouseholdQuestionnaireFormValidator(
+            cleaned_data=cleaned_data).clean()
+        return cleaned_data
 
     # TODO: Confirm the syntax
     class Meta:
